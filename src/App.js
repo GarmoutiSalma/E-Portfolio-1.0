76a692b1d4101f5a58aab5a4c4ff6184df66bb1d
Salma GARMOUTI <101300774+GarmoutiSalma@users.noreply.github.com>
2023-04-26 22:32:08 +0100

    deleting experience

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
-import { Experience } from "./components/Experience";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
 
@@ -16,7 +15,7 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
-      <Experience />
+    
       <Contact />
       <Footer />
     </div>
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ " a Software Engineer", "a Web Developer",  "an Artificial Intelligence Enthusiast" ];
+  const toRotate = [ " a Software Engineer", " a Web Developer",  " an Artificial Intelligence Enthusiast" ];
   const period = 2000;
 
   useEffect(() => {
