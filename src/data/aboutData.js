d961cd002dedb092692f39f7f287b053218b1ad7
Salma GARMOUTI <101300774+GarmoutiSalma@users.noreply.github.com>
2023-04-30 23:20:35 +0100

    ey

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -3,9 +3,9 @@ export const aboutData = {
     description1: "Hello! My name is Salma Garmouti. I'm an aspiring Software Engineer based in Morocco.",
     description2: `My name is Salma Garmouti and I am 
     a software engineer with a keen interest in space, astrophysics, 
-    and astronomy. From a young age, I have been captivated by the mysteries of the universe and the wonders of the cosmos.
-     My fascination with space has only grown as I have delved deeper into the fields of astrophysics and astronomy.As 
-     a software engineer, I am excited by the role that software and data analysis can play in advancing our understanding of the universe.
+    and astronomy. 
+    As 
+     a software engineer, I am excited by the role that software and data analysis can play in advancing our understanding of the cosmos.
       I am always seeking to learn more and pushing
       my boundaries , and I look forward to contributing to this exciting field in the future. `,
     image: 2
